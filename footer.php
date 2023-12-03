65876fe46b2f8acc1c2ed97ff8a8e8d61f9d8378
Amaan <A.GHULAM-DIN@lprs.fr>
2023-12-03 20:24:04 +0100

    Création d'un nouveau repertoire suite à l'ancien qui subissait des bugs.

---
+++ footer.php
@@ -0,0 +1,27 @@
+<footer id="footer">
+    <div class="inner">
+
+        <section>
+            <h2>Me contacter</h2>
+
+            <ul class="alt">
+                <li><a href="mailto:amaanali@outlook.fr">amaanali@outlook.fr</a></li>
+
+                <li><a href="mailto:A.GHULAM-DIN@lprs.fr">A.GHULAM-DIN@lprs.fr (professionnel)</a></li>
+
+                <li><span class="fa fa-phone"></span> 07 85 95 69 63 </li>
+            </ul>
+
+
+            <ul class="icons">
+                <li><a href="https://github.com/gdamaan/" class="icon style2 fa-github"><span class="label">Github</span></a></li>
+                <li><a href="https://www.linkedin.com/in/amaan-ghulam-din-aba520264/" class="icon style2 fa-linkedin"><span class="label">LinkedIn</span></a></li>
+                <li>Mon CV est téléchargeable <a href="cv/GHULAM_DIN_AMAAN_alternance_dev_1.pdf.198121508.pdf">ici</a></li>
+            </ul>
+        </section>
+
+        <ul class="copyright">
+            <li>Template by: <a href="https://www.phpjabbers.com/">PHPJabbers.com</a></li>
+        </ul>
+    </div>
+</footer>
